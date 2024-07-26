0fedc14136cb8abcc2adf7651c61613843783e66
junwon-hwnag <nyong1320@gmail.com>
2024-07-26 11:33:09 +0900

    # 인증 액션 디스패치

---
+++ src/redux-practice/components/Header.js
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Header.module.css";
+import { authActions } from "../store";
+
+
+const Header = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const dispatch = useDispatch();
+
+  const logOutHandler = e => {
+    dispatch(authActions.logOut());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isLoggedIn && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/public">My Products</a>
+            </li>
+            <li>
+              <a href="/public">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logOutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
--- src/redux-practice/store/index.js
+++ src/redux-practice/store/index.js
@@ -1,24 +1,17 @@
-import { configureStore , createSlice } from "@reduxjs/toolkit";
-
-// 액션 타입 상수
-// export const INCREMENT = 'INCREMENT';
-// export const DECREMENT = 'DECREMENT';
-// export const MULTIPLY = 'MULTIPLY';
-// export const TOGGLE = 'TOGGLE';
-
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 // 관리할 초기 상태값 객체
 const initialCountState = {
-  counter : 0,
-  showCounter : true,
+  counter: 0,
+  showCounter: true,
 };
 
-// reducer: 상태 변경을 위한 순수 함수 (부수 효과(비동기 코드...)가 없는 함수 )
+// reducer: 상태 변경을 위한 순수 함수 - 부수 효과(비동기코드...)가 없는 함수
 // 카운터 상태 관리를 위한 리듀서 정의
 /*
-  param1 (state): 상태 변경 이전의 상태  
-  param2 (action):  상태를 어떻게 변경할지의 대한 명세 
-  return : 변경 후 새로운 상태 값
+    param1 (state): 상태 변경 이전의 상태
+    param2 (action): 상태를 어떻게 변경할지의 대한 명세
+    return - 변경 후 새로운 상태값
 */
 
 // reducer를 slice로 변경
@@ -26,68 +19,61 @@ const initialCountState = {
   option객체에 들어가있는 프로퍼티 설명
   prop1: name - 컴포넌트가 해당 리듀서를 사용할 때 부르는 이름
   prop2: initialState - 관리할 상태값들의 초기값
-  prop3: reducers - 기존 리듀서에서 사용하던 내용들(실제 액션) 
+  prop3: reducers - 기존 리듀서에서 사용하던 내용들(실제 액션)
 */
 const counterSlice = createSlice({
   name: 'counter',
   initialState: initialCountState,
   reducers: {
-    increment(state){
+    increment(state) {
       state.counter++;
     },
-    decrement(state){
+    decrement(state) {
       state.counter--;
     },
-    multiyply(state,action){
+    multiply(state, action) {
+      // console.log('state: ', state);
+      // console.log('action: ', action);
       state.counter *= action.payload;
     },
-    toggle(state){
+    toggle(state) {
       state.showCounter = !state.showCounter;
     },
-  }  
+  }
+});
+
+// 인증 관련 초기 상태값
+const initialAuthState = {
+  isLoggedIn: false
+};
+
+// 인증 관련 슬라이스 추가
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logOut(state) {
+      state.isLoggedIn = false;
+    }
+  }
 });
 
 
-
-// const counterReducer = (state = initialCountState ,action) => {
-//   // 1. 상태값 변경시 새로운 상태를 반환
-//   // 2. 상태값 변경은 반드시 새로운 객체를 할당해야 함.
-//   switch(action.type){
-//     case INCREMENT:
-//       return {
-//         ...state,
-//         counter : state.counter + 1
-//       };
-//     case DECREMENT:
-//       return {
-//         ...state,
-//         counter : state.counter -1
-//       };
-//     case MULTIPLY:
-//       return {
-//         ...state,
-//         counter : state.counter * action.payload
-//       };
-//     case TOGGLE:
-//       return {
-//         ...state,
-//         showCounter : !state.showCounter
-//       }
-//     default:
-//       return state;
-//   }
-// };
-
-
-// 단 하나의 리덕스 스토어
-// 스토어에는 리듀서를 제공해야 한다.
+// 단하나의 리덕스 스토어
+// 스토어에는 여러 리듀서를 제공할 수 있다.
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 // 슬라이스 안에 reducers에 정의한 함수들을 내보내기
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
-
-// 리액트의 index.js에게 stroe를 제공
+// 리액트의 index.js에게 store를 제공
 export default store;
